0de9c1872cd4046a16d61e698157aeed97f9b7bc
lsh560 <lsh560@cs.utsarr.net>
2020-11-10 17:53:19 -0600

    Added main view for the GUI, and created inital models needed for the app

---
+++ src/application/model/Nutrition.java
@@ -0,0 +1,11 @@
+package application.model;
+
+public class Nutrition
+{
+	String amt_per_serving, serving_size, calories;
+	String total_fat, saturated_fat, cholesterol, sodium, total_carbs, dietary_fiber, sugar_amt, protein_amt;
+	public Nutrition()
+	{
+		
+	}
+}
---
+++ src/application/model/Recipe.java
@@ -0,0 +1,10 @@
+package application.model;
+
+public class Recipe
+{
+	private String title, category, serving_size, prep_time, cook_time;
+	public Recipe()
+	{
+		
+	}
+}
