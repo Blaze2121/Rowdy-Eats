15237dd9dc9388c4ee75ec30c1aff5740f1354a6
Brandon Bell <brandon.kristian.bell@gmail.com>
2020-11-23 20:30:14 -0600

    fixed another small issue

--- src/application/Main.java
+++ src/application/Main.java
@@ -20,7 +20,7 @@ public class Main extends Application {
 			Scene scene = new Scene(root);
 			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
-			MainController con = (MainController) loader.getController();
+			//MainController con = (MainController) loader.getController();
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
